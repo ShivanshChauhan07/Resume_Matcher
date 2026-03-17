9cddbba22c57654ca8f84e7c7577b36512349971
ShivanshChauhan07 <shiv3770@gmail.com>
2026-03-18 03:24:55 +0530

    streamLit update done

--- llm_utils.py
+++ llm_utils.py
@@ -1,10 +1,13 @@
 from google import genai
 from dotenv import load_dotenv
+import os
+import streamlit as st
 
 load_dotenv()
 
 def analyze_requirement_with_llm(prompt):
-    client = genai.Client()
+    api_key = st.secrets.get("GEMINI_API_KEY", os.getenv("GEMINI_API_KEY"))
+    client = genai.Client(api_key=api_key)
     response = client.models.generate_content(
         model='gemini-2.5-flash',
         contents=prompt
