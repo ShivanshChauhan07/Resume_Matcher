dfdc6fc1f95653b39d9a3ab8f598e9ec070c7a78
ShivanshChauhan07 <shiv3770@gmail.com>
2026-03-18 15:41:26 +0530

    llm change from gemini to groq

--- llm_utils.py
+++ llm_utils.py
@@ -1,15 +1,27 @@
-from google import genai
-from dotenv import load_dotenv
 import os
+from dotenv import load_dotenv
+from openai import OpenAI
 import streamlit as st
 
 load_dotenv()
 
 def analyze_requirement_with_llm(prompt):
-    api_key = st.secrets.get("GEMINI_API_KEY", os.getenv("GEMINI_API_KEY"))
-    client = genai.Client(api_key=api_key)
-    response = client.models.generate_content(
-        model='gemini-2.5-flash',
-        contents=prompt
+    api_key = st.secrets["GROQ_API_KEY"] if "GROQ_API_KEY" in st.secrets else os.getenv("GROQ_API_KEY")
+
+    if not api_key:
+        raise ValueError("GROQ_API_KEY not found in Streamlit secrets or environment variables.")
+   
+    client = OpenAI(
+        base_url="https://api.groq.com/openai/v1",
+        api_key=api_key
     )
-    return response.text
+
+    response = client.chat.completions.create(
+        model="llama-3.1-8b-instant",
+        messages=[
+            {"role": "user", "content": prompt}
+        ],
+        temperature=0.2
+    )
+
+    return response.choices[0].message.content
